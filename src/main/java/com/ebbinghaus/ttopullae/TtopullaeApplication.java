730feb26c6a2ca223c7b030092796161c62aa1a0
HyerimH <134906042+HyerimH@users.noreply.github.com>
2025-11-17 16:30:05 +0900

    Add files via upload

---
+++ src/main/java/com/ebbinghaus/ttopullae/TtopullaeApplication.java
@@ -0,0 +1,13 @@
+package com.ebbinghaus.ttopullae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtopullaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtopullaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ebbinghaus/ttopullae/TtopullaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebbinghaus.ttopullae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtopullaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
